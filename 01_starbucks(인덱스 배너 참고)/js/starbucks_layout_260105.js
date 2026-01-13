c61541ff7f8452c6b43c459ddcc3b534bcb5d437
uoahr <gkgkgk0525@gmail.com>
2026-01-13 13:12:30 +0900

    dd

---
+++ 01_starbucks(인덱스 배너 참고)/js/starbucks_layout_260105.js
@@ -0,0 +1,73 @@
+// starbucks_layout.js
+
+
+$(window).scroll(function () {
+    
+    //현재 스크롤 위치값 읽기!
+    var scTop = $(window).scrollTop();
+    console.log('scrollTop: ' + scTop);
+
+    //header 처리!
+    var headerHeight = $('header').innerHeight(); //66
+    console.log('header의 높잇값: ' + headerHeight);
+
+    if(scTop > headerHeight){
+        $('header').addClass('on');
+        $('section').css('margin-top', headerHeight);
+
+    } else {
+
+        $('header').removeClass('on');
+        $('section').css('margin-top', 0);오후 4:37 2026-01-05
+    }
+
+
+    var conbox = $('.content-box').offset().top; //966
+    console.log('content-box가 문서로부터 떨어진 거리: ' + conbox);
+
+    var gap = Math.ceil(conbox * 0.8); //773
+    console.log('gap: ' + gap);
+
+    // replay - stop(), animate() 메서드 사용 
+    // 이벤트가 발생하는 횟수만큼 핸들러가 실행, animate() 메서드가 대기열에 기다렸다가 실행되므로 조건식에 따라 바로 실행되지 않는 문제 발생 
+    // stop() 메서드로 진행중인 애니메이션 중단하고 현재 애니메이션 실행하도록 처리!
+
+    if (scTop > conbox - gap) {
+        //스크롤 위치값이 conbox(966)-gap(773)=193보다 클 때 실행!
+        $('.content-box > div.left').stop().animate({
+            left: 0
+        }, 400);
+        $('.content-box > div.right').stop().animate({
+            right: 0
+        }, 400);
+
+    } else {
+
+        $('.content-box > div.left').stop().animate({
+            left: '-50%'
+        }, 400);
+        $('.content-box > div.right').stop().animate({
+            right: '-50%'
+        }, 400);
+    }
+
+    // replay - on클래스 사용
+    /* if (scTop > conbox - gap){
+        $('.content-box > div').addClass('on');
+    }else{
+        $('.content-box > div').removeClass('on');
+    } */
+
+
+});
+
+$(document).ready(function () {
+
+    /* $('.viewbtn').click(function(){
+        $('html,body').animate({
+            scrollTop: conbox - gap //193
+        }, 800);
+    }); */
+
+});
+
